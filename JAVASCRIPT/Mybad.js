ed6d797eb55c9df7deee3ba234e0ee7d94767042
JEZZERR <blaqluv@icloud.com>
2025-01-19 01:03:53 +0000

    Add files via upload

---
+++ JAVASCRIPT/Mybad.js
@@ -0,0 +1,75 @@
+// console.log("LETS GET STARTED MY PEOPLE")
+// age = 23;
+// if (age ==45){
+//     console.log("This ocndiion is true ")
+// }else if(age <= 15){
+//     console.log("This is true")
+// }else if (age <=3){
+//     console.log("This condition is true")
+// }else
+// {console.log("all the conditions are false")}
+ 
+// // USING CONDITION IF 
+// grade = 2.50;
+// if (grade >= 3.00){
+//     console.log("You had B")
+// }else if (grade >=3.50){
+//     console.log("You had B+")
+// }else if(grade <= 2.50){
+//     console.log("You had C+")
+// }else
+// { console.log("Attempted degree .. Go home!!")}
+
+// //USING SWITCH CASE 
+//   grade = "D"
+// switch(grade){
+//     case "A":
+//     console.log("You had a first class")
+//     break;
+
+//     case "B+":
+//     console.log("You had a second class upper ")
+//     break;
+
+//     case "B":
+//     console.log("You had a second class lower")
+//     break;
+
+//     case "c+":
+//     console.log("You had a third class upper")
+//     break;
+
+//     case "D":
+//     console.log("Your grade is not good, try harder next semester please ")
+//     break;
+// }
+
+// // IF CONDITIONS 
+//  Ccode = "DCIT104"
+//  lastValue =Ccode[6]
+//  if(lastValue % 2 == 0){
+//     console.log("is a second semester course")
+//  }
+//  else 
+//  {
+//     console.log("Is a first semester course")
+//  }
+
+//  year = 2004 
+//  YourAge = 2024 - year 
+//  console.log("You are :",YourAge,"years")
+//  console.log("Your favourite 6 even numbers are :")
+//  // USING FOR LOOP
+//  let i;
+//  for (i=0; i <=10 ; i= i+2){
+//     console.log(i)
+// }
+//  Ama = "Girl"
+//  Kofi = "Boy"
+//  Relation = "the relationship between Ama and Kofi"
+//  console.log("Ama is the sister of Kofi")
+
+// assignment one 
+console.log ("HELLO WORLD")
+
+
